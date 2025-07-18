b92d2f30c66679e611370e2c6b62793224be58c6
kaiqueqg <6303515+kaiqueqg@users.noreply.github.com>
2025-07-18 16:41:10 +0200

    ;

--- src/Main/Curriculum/Curriculum.tsx
+++ src/Main/Curriculum/Curriculum.tsx
@@ -12,17 +12,17 @@ const Curriculum: React.FC<CurriculumProps> = () => {
     <div className='curriculumContainer'>
       <div className='cvTopItemsContainer'>
         <div className='cvLangContainer'>
-          <img src={process.env.PUBLIC_URL + '/brazil.png'} alt='' className={lang !== Language.PR_BR? 'cvLangImg' : 'cvLangImgSelected'} onClick={() => {setLang(Language.PR_BR)}}></img>
+          {/* <img src={process.env.PUBLIC_URL + '/brazil.png'} alt='' className={lang !== Language.PR_BR? 'cvLangImg' : 'cvLangImgSelected'} onClick={() => {setLang(Language.PR_BR)}}></img> */}
           <img src={process.env.PUBLIC_URL + '/usa.png'} alt='' className={lang !== Language.EN? 'cvLangImg' : 'cvLangImgSelected'} onClick={() => {setLang(Language.EN)}}></img>
           <img src={process.env.PUBLIC_URL + '/france.png'} alt='' className={lang !== Language.FR? 'cvLangImg' : 'cvLangImgSelected'} onClick={() => {setLang(Language.FR)}}></img>
-          <img src={process.env.PUBLIC_URL + '/italy.png'} alt='' className={lang !== Language.IT? 'cvLangImg' : 'cvLangImgSelected'} onClick={() => {setLang(Language.IT)}}></img>
+          {/* <img src={process.env.PUBLIC_URL + '/italy.png'} alt='' className={lang !== Language.IT? 'cvLangImg' : 'cvLangImgSelected'} onClick={() => {setLang(Language.IT)}}></img> */}
         </div>
       </div>
       <div className='cvContainer'>
-        {lang === Language.PR_BR && <iframe className='pdf' src={process.env.PUBLIC_URL +  "/Curriculum.pdf#view=fitH"}/>}
+        {/* {lang === Language.PR_BR && <iframe className='pdf' src={process.env.PUBLIC_URL +  "/Curriculum.pdf#view=fitH"}/>} */}
         {lang === Language.EN && <iframe className='pdf' src={process.env.PUBLIC_URL +  "/Curriculum-En.pdf#view=fitH"}/>}
         {lang === Language.FR && <iframe className='pdf' src={process.env.PUBLIC_URL +  "/Curriculum-Fr.pdf#view=fitH"}/>}
-        {lang === Language.IT && <iframe className='pdf' src={process.env.PUBLIC_URL +  "/Curriculum-It.pdf#view=fitH"}/>}
+        {/* {lang === Language.IT && <iframe className='pdf' src={process.env.PUBLIC_URL +  "/Curriculum-It.pdf#view=fitH"}/>} */}
         {/* <Knowledges lang={lang}></Knowledges>
         <Experiences lang={lang}></Experiences> */}
       </div>
